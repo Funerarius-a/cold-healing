895ede5ed1e5ec6d422ff2e534dcdf4f51f031d9
Funerarius-a <anthoniluidy@gmail.com>
2026-05-11 21:47:14 -0300

    Small tweaks for the 1.0 release

--- src/main/java/net/funerarius/coldhealing/ModConfigs.java
+++ src/main/java/net/funerarius/coldhealing/ModConfigs.java
@@ -47,97 +47,97 @@ public class ModConfigs {
 
     static {
         BUILDER.push("AFAK Settings");
-        AFAK_STAGE1_REMOVE = BUILDER.comment("Effects that are removed by the first stage of the AFAK")
+        AFAK_STAGE1_REMOVE = BUILDER.comment("List of effects cleared by the first stage of the AFAK")
                 .defineList("afak_stage1_remove", List.of("minecraft:wither", "minecraft:poison"), obj -> true);
 
-        AFAK_STAGE1_ADD = BUILDER.comment("Effects added by the first stage of the AFAK. Format: \"mod:effect, amplifier, duration_ticks\"")
+        AFAK_STAGE1_ADD = BUILDER.comment("Effects applied by the first stage of the AFAK. Format: \"mod:effect, amplifier, duration_ticks\"")
                 .defineList("afak_stage1_add", List.of(), obj -> true);
 
-        AFAK_STAGE2_REMOVE = BUILDER.comment("Effects that are removed by the second stage of the AFAK")
+        AFAK_STAGE2_REMOVE = BUILDER.comment("List of effects cleared by the second stage of the AFAK")
                 .defineList("afak_stage2_remove", List.of(), obj -> true);
 
-        AFAK_STAGE2_ADD = BUILDER.comment("Effects added by the second stage of the AFAK. Format: \"mod:effect, amplifier, duration_ticks\"")
+        AFAK_STAGE2_ADD = BUILDER.comment("Effects applied by the second stage of the AFAK. Format: \"mod:effect, amplifier, duration_ticks\"")
                 .defineList("afak_stage2_add", List.of("minecraft:regeneration, 1, 100"), obj -> true);
         BUILDER.pop();
 
         BUILDER.push("IFAK Settings");
-        IFAK_REMOVE = BUILDER.comment("Effects removed by the IFAK")
+        IFAK_REMOVE = BUILDER.comment("List of effects cleared by the IFAK")
                 .defineList("ifak_remove", List.of("minecraft:wither"), obj -> true);
 
-        IFAK_ADD = BUILDER.comment("Effects added by the IFAK. Format: \"mod:effect, amplifier, duration_ticks\"")
+        IFAK_ADD = BUILDER.comment("Effects applied by the IFAK. Format: \"mod:effect, amplifier, duration_ticks\"")
                 .defineList("ifak_add", List.of(), obj -> true);
         BUILDER.pop();
 
         BUILDER.push("Ibuprofen Settings");
-        IBUPROFEN_REMOVE = BUILDER.comment("Effects removed by the Ibuprofen")
+        IBUPROFEN_REMOVE = BUILDER.comment("List of effects cleared by the Ibuprofen")
                 .defineList("ibuprofen_remove", List.of("minecraft:poison", "minecraft:darkness"), obj -> true);
 
-        IBUPROFEN_ADD = BUILDER.comment("Effects added by the Ibuprofen. Format: \"mod:effect, amplifier, duration_ticks\"")
-                .defineList("ibuprofen_add", List.of("minecraft:weakness, 0, 800", "minecraft:mining_fatigue, 0, 200"), obj -> true);
+        IBUPROFEN_ADD = BUILDER.comment("Effects applied by the Ibuprofen. Format: \"mod:effect, amplifier, duration_ticks\"")
+                .defineList("ibuprofen_add", List.of("minecraft:weakness, 0, 800", "minecraft:mining_fatigue, 0, 200", "legendarysurvivaloverhaul:painkiller, 0, 2600", "legendarysurvivaloverhaul:painkiller_addiction, 0, 3200"), obj -> true);
 
-        IBUPROFEN_INCOMPATIBLE = BUILDER.comment("List of effects that when present makes the ibuprofen unusable")
+        IBUPROFEN_INCOMPATIBLE = BUILDER.comment("List of effects that prevent Ibuprofen from being used")
                 .defineList("ibuprofen_incompatible", List.of("minecraft:weakness"), obj -> true);
         BUILDER.pop();
 
         BUILDER.push("Splint Settings");
-        SPLINT_REMOVE = BUILDER.comment("Effects removed by the Splint")
+        SPLINT_REMOVE = BUILDER.comment("List of effects cleared by the Splint")
                 .defineList("splint_remove", List.of("minecraft:slowness"), obj -> true);
 
-        SPLINT_ADD = BUILDER.comment("Effects added by the Splint. Format: \"mod:effect, amplifier, duration_ticks\"")
+        SPLINT_ADD = BUILDER.comment("Effects applied by the Splint. Format: \"mod:effect, amplifier, duration_ticks\"")
                 .defineList("splint_add", List.of("minecraft:mining_fatigue, 0, 200"), obj -> true);
 
-        SPLINT_INCOMPATIBLE = BUILDER.comment("List of effects that when present makes the splint unusable")
+        SPLINT_INCOMPATIBLE = BUILDER.comment("List of effects that prevent the Splint from being used")
                 .defineList("splint_incompatible", List.of("minecraft:wither", "minecraft:poison"), obj -> true);
         BUILDER.pop();
 
         BUILDER.push("CMS Settings");
-        CMS_REMOVE = BUILDER.comment("Effects removed by the CMS")
+        CMS_REMOVE = BUILDER.comment("List of effects cleared by the CMS surgery kit")
                 .defineList("cms_remove", List.of("minecraft:slowness"), obj -> true);
 
-        CMS_ADD = BUILDER.comment("Effects added by the CMS. Format: \"mod:effect, amplifier, duration_ticks\"")
+        CMS_ADD = BUILDER.comment("Effects applied by the CMS. Format: \"mod:effect, amplifier, duration_ticks\"")
                 .defineList("cms_add", List.of("minecraft:mining_fatigue, 0, 800", "minecraft:regeneration, 1, 60"), obj -> true);
         BUILDER.pop();
 
         BUILDER.push("P-04 Stabilizer Settings");
-        P04_REMOVE = BUILDER.comment("Effects removed by the P-04")
+        P04_REMOVE = BUILDER.comment("List of effects cleared by the P-04 Stabilizer")
                 .defineList("p_04_remove", List.of("minecraft:wither"), obj -> true);
 
-        P04_ADD = BUILDER.comment("Effects added by the S3. Format: \"mod:effect, amplifier, duration_ticks\"")
+        P04_ADD = BUILDER.comment("Effects applied by the P-04 Stabilizer. Format: \"mod:effect, amplifier, duration_ticks\"")
                 .defineList("p_04_add", List.of("minecraft:mining_fatigue, 0, 800", "minecraft:regeneration, 1, 60"), obj -> true);
         BUILDER.pop();
 
         BUILDER.push("Adreno Neural S3 Settings");
-        ADRENO_REMOVE = BUILDER.comment("Effects removed by the S3")
+        ADRENO_REMOVE = BUILDER.comment("List of effects cleared by the Adreno Neural S3")
                 .defineList("adreno_remove", List.of("minecraft:slowness"), obj -> true);
 
-        ADRENO_ADD = BUILDER.comment("Effects added by the S3. Format: \"mod:effect, amplifier, duration_ticks\"")
+        ADRENO_ADD = BUILDER.comment("Effects applied by the Adreno Neural S3. Format: \"mod:effect, amplifier, duration_ticks\"")
                 .defineList("adreno_add", List.of("minecraft:speed, 0, 800"), obj -> true);
         BUILDER.pop();
 
         BUILDER.push("Hematox-G Settings");
-        HEMATOX_REMOVE = BUILDER.comment("Effects removed by the Hematox")
+        HEMATOX_REMOVE = BUILDER.comment("List of effects cleared by the Hematox-G")
                 .defineList("hematox_remove", List.of("minecraft:poison"), obj -> true);
 
-        HEMATOX_ADD = BUILDER.comment("Effects added by the Hematox. Format: \"mod:effect, amplifier, duration_ticks\"")
+        HEMATOX_ADD = BUILDER.comment("Effects applied by the Hematox-G. Format: \"mod:effect, amplifier, duration_ticks\"")
                 .defineList("hematox_add", List.of("minecraft:regeneration, 0, 1200"), obj -> true);
         BUILDER.pop();
 
         BUILDER.push("S9 N-BLOCK Settings");
-        NBLOCK_REMOVE = BUILDER.comment("Effects removed by the S9")
+        NBLOCK_REMOVE = BUILDER.comment("List of effects cleared by the S9 N-BLOCK")
                 .defineList("s9_remove", List.of("minecraft:blindness", "minecraft:darkness"), obj -> true);
 
-        NBLOCK_ADD = BUILDER.comment("Effects added by the S9. Format: \"mod:effect, amplifier, duration_ticks\"")
-                .defineList("s9_add", List.of("minecraft:weakness, 0, 800"), obj -> true);
+        NBLOCK_ADD = BUILDER.comment("Effects applied by the S9 N-BLOCK. Format: \"mod:effect, amplifier, duration_ticks\"")
+                .defineList("s9_add", List.of("minecraft:weakness, 0, 800", "legendarysurvivaloverhaul:painkiller, 0, 2000", "legendarysurvivaloverhaul:painkiller_addiction, 0, 3000"), obj -> true);
         BUILDER.pop();
 
         BUILDER.push("Car Kit Settings");
-        CAR_HEAL_ADD = BUILDER.comment("Effects added by car heal mode")
+        CAR_HEAL_ADD = BUILDER.comment("Effects applied by the CAR kit in Heal Mode")
                 .defineList("car_heal_add", List.of("minecraft:regeneration, 0, 200"), obj -> true);
 
-        CAR_BANDAGE_REMOVE = BUILDER.comment("Effects removed by the car bandage mode")
+        CAR_BANDAGE_REMOVE = BUILDER.comment("List of effects cleared by the CAR kit in Bandage Mode")
                 .defineList("car_bandage_remove", List.of("minecraft:wither", "minecraft:poison"), obj -> true);
 
-        CAR_BANDAGE_ADD = BUILDER.comment("Effects added by the car bandage mode")
+        CAR_BANDAGE_ADD = BUILDER.comment("Effects applied by the CAR kit in Bandage Mode")
                 .defineList("car_bandage_add", List.of(), obj -> true);
         BUILDER.pop();
 
